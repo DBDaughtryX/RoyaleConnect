6846daab4f2a1b5276f4b62ac418be23fbf8e6dd
DillonBD <146242527+DBDaughtryX@users.noreply.github.com>
2023-09-27 13:01:30 -0500

    Initial Commit

---
+++ RoyaleConnect/RoyaleConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoyaleConnectApp.swift
+//  RoyaleConnect
+//
+//  Created by Dillon Borden on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct RoyaleConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
