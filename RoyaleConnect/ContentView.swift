ca919209976d6215ab2d187344712fa37e95d0b9
DillonBD <146242527+DBDaughtryX@users.noreply.github.com>
2023-09-27 15:17:24 -0500

    Added a Login_ UI
    Added a LoginUI, will be adjusting overtime

--- RoyaleConnect/ContentView.swift
+++ RoyaleConnect/ContentView.swift
@@ -8,10 +8,107 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userName = ""
+    @State private var password = ""
+    @State private var clashTag = ""
+    
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var invalidSuperCellID = 0
+    
+    @State private var showingLoginScreen = false
+    
+    @State private var animatedGradient = false
     
     
     var body: some View {
-        Text("Test")
+            NavigationView{
+                ZStack{
+                    LinearGradient(gradient: Gradient(colors: [Color.blue, Color.green]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                        .ignoresSafeArea()
+                        .hueRotation(.degrees(animatedGradient ? 45 : 0))
+                        .onAppear{
+                            withAnimation(.easeInOut(duration: 3.0).repeatForever(autoreverses: true)){
+                                animatedGradient.toggle()
+                            }
+                        }
+                    Circle()
+                        .scale(1.35)
+                        .foregroundColor(.white.opacity(0.15))
+                    Circle()
+                        .scale(1.35)
+                        .foregroundColor(.black)
+                    VStack{
+                        Text("Username")
+                            .foregroundColor(.black)
+                            .offset(x: -900)
+                            .font(.largeTitle)
+                            .bold()
+                            .padding()
+                            .padding()
+                        
+                        Text("Username: ")
+                            .foregroundColor(.white)
+                            .offset(x: -100)
+                        
+                        TextField("Username", text: $userName)
+                            .padding()
+                            .frame(width: 300, height: 50)
+                            .background(Color.white.opacity(1))
+                            .foregroundColor(.black)
+                            .cornerRadius(10)
+                            .border(.red, width: CGFloat(wrongUsername))
+                            .offset(y: 10)
+                        
+                            Text("Password: ")
+                            .foregroundColor(.white)
+                            .offset(x: -110, y: 22)
+                            
+                        
+                        SecureField("Password", text: $password)
+                            .padding()
+                            .frame(width: 300, height: 50)
+                            .foregroundColor(.black)
+                            .background(Color.white.opacity(1))
+                            .cornerRadius(10)
+                            .border(.red, width: CGFloat(wrongPassword))
+                            .padding()
+                        
+                        Text("Clash royale #: ")
+                            .foregroundColor(.white)
+                            .offset(x: -99)
+                            .font(.subheadline)
+                            .bold()
+                        
+                        TextField("Clash royale Tag#", text: $clashTag)
+                            .padding()
+                            .frame(width: 300, height: 50)
+                            .background(Color.white.opacity(1))
+                            .foregroundColor(.black )
+                            .cornerRadius(10)
+                            .border(.red, width: CGFloat(invalidSuperCellID))
+                        
+                        
+                    }
+                    Image("logo2")
+                        .resizable()
+                        .frame(width: 170, height: 130)
+                        .offset(y: -185)
+                    
+                    Text("RoyaleConnect")
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .bold()
+                    
+                        .offset(y:-140)
+                    
+                        Spacer()
+                        
+                }
+                
+                
+            }
+        .navigationBarHidden(true)
     }
 
 }
